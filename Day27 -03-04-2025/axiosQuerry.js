c9ad5f9d413be0b761cea9beab7dd1a6c17422da
honey8899 <amansrivastava8778@gmail.com>
2025-04-03 18:23:15 +0530

    :tada: Comppleted JavaScript

--- Day27 -03-04-2025/axiosQuerry.js
+++ Day27 -03-04-2025/axiosQuerry.js
@@ -1,14 +1,36 @@
+let btn = document.querySelector("button");  
+
 const url= "http://universities.hipolabs.com/search?name=";
 
-let country="nepal";
-
-async function getColleges(){
+async function getColleges(country){
     try{
         let res= await axios.get(url+country);
+        console.log(res.data);
 
-        console.log(res); 
+        return res.data;
     }
     catch(e){
         console.log(e);
+        return [];
     }
 }
+
+btn.addEventListener("click",async () => {
+    let countryName = document.getElementById("country").value;
+    let colArr=await getColleges(countryName);
+    show(colArr);
+});
+
+function show(colArr){
+    let list=document.querySelector("#list");
+    list.innerText="";
+
+    for(col of colArr){
+        console.log(col.name);
+
+        let li=document.createElement("li");
+        li.innerText=col.name;
+        list.appendChild(li);
+    }
+}
+
